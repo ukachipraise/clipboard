4cbcfa53ddfc050262b602c148b7e1c324c42d0b
ukachipraise <ukachipraise80@gmail.com>
2025-10-02 09:26:12 +0100

    addad scrolling animation

---
+++ config.js
@@ -0,0 +1,24 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Bai Jamjuree', 'sans-serif'],
+          },
+          colors: {
+            green: {
+              500: 'hsl(171, 66%, 44%)',
+            },
+            blue: {
+              100: 'hsl(233, 100%, 69%)',
+            },
+            gray: {
+              700: 'hsl(210, 10%, 33%)',
+              500: 'hsl(201, 11%, 66%)',
+            },
+          },
+          fontSize: {
+            base: '18px',
+          },
+        }
+      }
+    }
